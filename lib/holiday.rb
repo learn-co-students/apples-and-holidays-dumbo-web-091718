9bfc94ed93269f94d50a70fdd67d88e3c5abbaaf
Cristina Murillo <cmurill1@jhu.edu>
2018-08-21 00:15:38 +0000

    Done.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -18,12 +18,15 @@ def second_supply_for_fourth_of_july(holiday_hash)
   #   }
   # }
   # return the second element in the 4th of July array
+  holiday_hash[:summer][:fourth_of_july][1]
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
   # holiday_hash is identical to the one above
   # add the second argument, which is a supply, to BOTH the
   # Christmas AND the New Year's arrays
+  holiday_supplies[:winter][:christmas] << supply 
+  holiday_supplies[:winter][:new_years] << supply
 
 end
 
@@ -31,17 +34,22 @@ end
 def add_supply_to_memorial_day(holiday_hash, supply)
   # again, holiday_hash is the same as the ones above
   # add the second argument to the memorial day array
+  holiday_hash[:spring][:memorial_day] << supply 
 
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   # code here
   # remember to return the updated hash
+  holiday_hash[season][holiday_name] = supply_array
+  
 
 end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
+  array = holiday_hash[:winter].values 
+  array.flatten 
 
 end
 
@@ -53,13 +61,27 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
+  holiday_hash.each do |season, value|
+      puts "#{season.capitalize}:"
+    value.each do |holiday, supplies|
+      holiday_string = holiday.to_s 
+      holiday_array = holiday_string.split("_")
+      capital_holidays = holiday_array.collect do |item|
+        item.capitalize
+      end  
+      holiday_string = capital_holidays.join(" ")
+      puts "  #{holiday_string}: #{supplies.join(", ")}"
+    end 
+  end
 
 end
 
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-
+  array = holiday_hash[:summer].keys
+  array << holiday_hash[:spring].keys
+  array.flatten 
 end
 
 
