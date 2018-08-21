6c5e78add32631611638f4f1257a9d6836fe39df
Cristina Murillo <cmurill1@jhu.edu>
2018-08-21 00:17:09 +0000

    Done.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -79,8 +79,7 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-  array = holiday_hash[:summer].keys
-  array << holiday_hash[:spring].keys
+  array = [holiday_hash[:summer].keys, holiday_hash[:spring].keys]
   array.flatten 
 end
 
