1f89898e8750606a401d04df6380c4e871981934
Nasser Sanou <nassersanou23@gmail.com>
2018-08-31 04:56:00 +0000

    Done.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -59,12 +59,13 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-
+    holiday_hash[]
 end
 
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
+  
 
 end
 
